ee1b022533e9be6bf86af39fa3d1ffff76214c85
Rameen Ejaz <rameenejaz4@gmail.com>
2025-03-15 23:55:56 +0500

    using validations to find time

---
+++ Time.java
@@ -0,0 +1,20 @@
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    Time() {
+        hours=0;
+        minutes=0;
+        seconds=0;
+    }
+    Time(int inHours, int inMinutes, int inSeconds) {
+        hours=inHours;
+        minutes=inMinutes;
+        seconds=inSeconds;
+    }
+    // public void d
+    public static void main(String[] args) {
+        Time calculateTime= new Time();
+        
+    }
+}
