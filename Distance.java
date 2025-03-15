57bc3b9bc745249ce74fcf470a5cb78ef47056ea
Rameen Ejaz <rameenejaz4@gmail.com>
2025-03-15 23:35:02 +0500

    finding the distance in feet and inches

--- Distance.java
+++ Distance.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Distance {
+    private double feet;
+    private double inches;
+    Distance () {
+        feet=1;
+        inches=1;
+    }
+    Distance(double inputFeet, double inputInches ) {
+        feet=inputFeet;
+        inches=inputInches;
+    }
+    public void getFeet(double inputFeet) {
+        System.out.printf("\nEnter length in feet: %.2f\n", inputFeet);
+    }
+    public void getFeet(double inputInches) {
+        System.out.printf("\nEnter length in feet: %.2f\n", inputInches);
+    }
+    public void display() {
+        System.out.printf("\nThe default distance in feet is:", feet);
+        System.out.printf("\nThe default distance in inches is:" ,inches);
+    }
     public static void main(String[] args) {
-        
+        Scanner input= new Scanner(System.in);
+        Distance measurement=new Distance();
+       measurement.display();
+
+
     }
 }
