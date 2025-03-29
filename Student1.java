7b3331c1c5fb08a5fc22a210041797391f51cf9b
Rameen Ejaz <rameenejaz4@gmail.com>
2025-03-29 16:16:39 +0500

    course enrollment system

--- Student1.java
+++ Student1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Student1 {
     private int studentID;
     private String name;
@@ -13,8 +14,9 @@ public class Student1 {
         this.studentID=studID;
         this.name=studName;
         this.enrolledCount=count;
+        this.enrolledCourses=new String[5];
     }
-    public void enrolledCourses(String courseName) {
+    public void enrollCourses(String courseName) {
         if (enrolledCount>=enrolledCourses.length) {
             System.out.println(name + " no more courses can be registered, limit reached!");
             return;
@@ -22,11 +24,13 @@ public class Student1 {
         for (int i=0; i<enrolledCount; i++) {
             if (enrolledCourses[i].equals(courseName)) {
                 System.out.println(name +" has already been enrolled in " + courseName);
+                return;
             }
         }
         enrolledCourses[enrolledCount]=courseName;
         enrolledCount++;
-        System.out.println(name + " has successfully enrolled in the " + courseName);
+        System.out.println();
+        System.out.println(name + " has successfully enrolled in " + courseName);
     }
     public void dropCourse (String courseName) {
         if (enrolledCount<=0) {
@@ -45,6 +49,7 @@ public class Student1 {
                 enrolledCourses[enrolledCount-1]=null;
                 enrolledCount--;
                 System.out.println(name + " has successfully removed " + courseName);
+                return;
             }
         }
         if (!found) {
@@ -58,10 +63,66 @@ public class Student1 {
         else {
             System.out.println(name + " registered in rhe following courses:");
             for (int i=0; i<enrolledCount; i++) {
-                System.out.println(enrolledCourses[i] + "-");
+                System.out.print(enrolledCourses[i] + "-");
             }
-            System.out.println();
         }
+    }
+    public void displayMenu() {
+        System.out.println("--------------------------------------");
+        System.out.println("      COURSE ENROLLMENT SYSTEM        ");
+        System.out.println("\n1. To add a course\n");
+        System.out.println("\n2. To drop a course\n");
+        System.out.println("\n3. To display list of courses\n");
+        System.out.println("--------------------------------------");
+        System.out.print("\nChoose from the following options (1-3) or 0 to exit:");
+    }
+    public static void main(String[] args) {
+        // Student1 student=new Student1();
+        Scanner input=new Scanner(System.in);
+        int option=-99;
+        int ID=0;
+        String name1="";
+        System.out.print("Enter your student ID:");
+            ID=input.nextInt();
+            input.nextLine();
+            System.out.print("Enter your name:");
+            name1=input.nextLine();
+            Student1 students=new Student1(ID, name1,0);
+            // student(ID, name1);
+        while (option!=0) {
+            students.displayMenu();
+            option=input.nextInt();
+            switch(option) {
+                case 1:
+                input.nextLine();
+                System.out.print("\nEnter name of the course you want to enroll in:");
+                String inputCourseEnrollName=input.nextLine();
+                students.enrollCourses(inputCourseEnrollName);
+                System.out.println("DONE COURSE ENROLLED");
+                break;
+
+                case 2:
+                System.out.println("Enter name of the course you want to drop:");
+                input.nextLine();
+                String inputCourseDropName=input.nextLine();
+                students.dropCourse(inputCourseDropName);
+                System.out.println("\nDONE COURSE DROPPED");
+                break;
+
+                case 3:
+                students.displayCourses();
+                System.out.println("\nDONE COURSE DISPLAYED");
+                break;
+
+                case 0:
+                System.out.println("\nEXITING THE PROGRAM!");
+                break;
+
+                default:
+                System.out.println("invalid input");
+            }
+        }
+        input.close();
     }
 }
 
