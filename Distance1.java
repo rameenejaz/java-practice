4bd5ef0bc778b50f0e9285ff5c2b32ea939c12ff
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-04 02:39:57 +0500

    adding two distances

--- Distance1.java
+++ Distance1.java
@@ -27,9 +27,21 @@ public class Distance1 {
     public Distance1 add(Distance1 d) {
         int totalFeet=this.feet+d.feet;
         int totalInches=this.inches+d.inches;
-        return new(totalFeet, totalInches);
+        return new Distance1(totalFeet, totalInches);
+    }
+    public void convert() {
+        if (inches>=12) {
+            feet+=(inches/12);
+            inches=inches%12;
+        }
     }
     public static void main(String[] args) {
-        
+        Distance1 d1=new Distance1(5,9);
+        Distance1 d2=new Distance1(4,5);
+        d1.display();
+        d2.display();
+        Distance1 sum=d1.add(d2);
+        sum.display();
+
     }
 }
