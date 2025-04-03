ea8043723b2b30c9ef2f843e16e72789d11f74e4
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-03 21:56:59 +0500

    adding two objects

---
+++ Distance1.java
@@ -0,0 +1,18 @@
+public class Distance1 {
+    private int feet;
+    private int inches;
+    Distance1() {
+        feet=0;
+        inches=0;
+    }
+    Distance1(int feet, int inches) {
+        this.feet=feet;
+        this.inches=inches;
+    }
+    public void getInches() {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
