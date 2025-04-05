d349999a8b38fa461b4de884da9eb6f14bfbd647
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-06 00:08:16 +0500

    comparing the authors of two books

--- Book1.java
+++ Book1.java
@@ -7,26 +7,36 @@ public class Book1 {
     }
     public Book1(String author, String chapterNames[]) {
         this.author=author;
-        for(int i=0; i<100; i++) {
+        for(int i=0; i<chapterNames.length; i++) {
             this.chapterNames[i]=chapterNames[i];
         }
     }
-    public Book1 compareBooks(Book1 secondBook) {
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author=author;
+    }
+    public String [] getChapterNames() {
+        return chapterNames;
+    }
+    public void setChapterNames(String [] chapterName) {
+        for(int i=0; i<chapterName.length; i++) {
+            this.chapterNames[i]=chapterName[i];
+        }
+    }
+    public boolean compareBooks(Book1 secondBook) {
         boolean isSame=false;
         author.toLowerCase();
         secondBook.author.toLowerCase();
+        // return author.equals(secondBook.author);
         if(author==secondBook.author) {
             isSame=true;
         }
         else {
             isSame=false;
         }
-        return new Book1(author,chapterNames);
-        // bool isSame=false;
-        // author1.toLowerCase();
-        // author2.toLowerCase();
-        // if(author1==author2) {
-        //     isSame=true;
-        // }
+        return isSame;
     }
+
 }
