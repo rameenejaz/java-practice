1a114424b6f9dcdbf8a0a6c963dfc7b4ab069725
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-22 15:43:14 +0500

    extending a new class and making default constructors

---
+++ Faculty.java
@@ -0,0 +1,8 @@
+public class Faculty extends Employee {
+    private int officeHours;
+    private int rank;
+    public Faculty() {
+        officeHours=0;
+        rank=0;
+    }
+}
---
+++ Staff.java
@@ -0,0 +1,7 @@
+public class Staff extends Employee {
+    private String title;
+    public Staff () {
+        title="";
+    }
+    
+}
