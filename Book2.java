6aab7e77fd4cab7acbf24fa1902c9c6dfa5a3b70
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-24 23:15:34 +0500

    adding a page counter

--- Book2.java
+++ Book2.java
@@ -1,9 +1,11 @@
 public class Book2 {
     private String title;
     private double price;
+    private int counter;
     public Book2() {
         title="";
         price=0.0;
+        counter=0;
     }
     public String getBookTitle() {
         return title;
@@ -17,6 +19,10 @@ public class Book2 {
     public void setBookPrice(double price) {
         this.price=price;
     }
+    public void pageCount() {
+        counter++;
+    }
     
+
 
 }
