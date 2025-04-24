b97f5f25333cf625ea0a2b7e995c59abbd58033e
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-24 23:28:55 +0500

    making get and set functions

--- Tape2.java
+++ Tape2.java
@@ -1,3 +1,23 @@
 public class Tape2 {
+    private String tapeTitle;
+    private double tapePrice;
+    private int tapeCounter;
+    public Tape2() {
+        tapeTitle="";
+        tapePrice=0.0;
+        tapeCounter=0;
+    }
+    public String getTapeTitle() {
+        return tapeTitle;
+    }
+    public void setTapeTitle(String title) {
+        this.tapeTitle=title;
+    }
+    public double getTapePrice() {
+        return tapePrice;
+    }
+    public void setTapePrice(double price) {
+        this.tapePrice=price;
+    }
     
 }
