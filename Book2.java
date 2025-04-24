e26f6c7108c3cd83f4f06780457b328bd00c4e9a
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-24 22:59:46 +0500

    making  a book class with its specific methods

---
+++ Book2.java
@@ -0,0 +1,22 @@
+public class Book2 {
+    private String title;
+    private double price;
+    public Book2() {
+        title="";
+        price=0.0;
+    }
+    public String getBookTitle() {
+        return title;
+    }
+    public void setBookTitle(String title) {
+        this.title=title;
+    }
+    public double getBookPrice () {
+        return price;
+    }
+    public void setBookPrice(double price) {
+        this.price=price;
+    }
+    
+
+}
