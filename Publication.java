bdcc8cc6dec9e3ec86630e2be753770f79fd5dbb
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-24 23:43:03 +0500

    making a parent class with functions

---
+++ Publication.java
@@ -0,0 +1,17 @@
+public class Publication {
+    private String title;
+    private double price;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title=title;
+    }
+    public double getPrice () {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price=price;
+    }
+    
+}
