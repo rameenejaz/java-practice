599bdc052a56af2ea827501c1c5eecf3a125678b
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-25 12:59:26 +0500

    defining the attributes

---
+++ Computer.java
@@ -0,0 +1,6 @@
+public class Computer {
+    protected int wordSize; //in bits
+    protected int memorySize; //in megabytes
+    protected int storageSize; //in megabytes
+    protected int speed; //in megahertz
+}
