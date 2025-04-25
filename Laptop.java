c9e5d8bf2ca408798e8b4c261f9a7d8bf6b0a193
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-25 13:08:05 +0500

    making constructors in the derived class

--- Laptop.java
+++ Laptop.java
@@ -3,5 +3,18 @@ public class Laptop extends Computer{
     private int width;
     private int height;
     private int weight;
-    
+    public Laptop() {
+        super();
+        length=0;
+        width=0;
+        height=0;
+        weight=0;
+    }
+    public Laptop(int wordSize, int memorySize, int storageSize, int speed, int length, int width, int height, int weight) {
+        super(wordSize, memorySize,storageSize, speed);
+        this.length=length;
+        this.width=width;
+        this.height=height;
+        this.weight=weight;
+    }
 }
