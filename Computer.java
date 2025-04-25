f2e7be3721f53b1dd78ae814075919d088434816
Rameen Ejaz <rameenejaz4@gmail.com>
2025-04-25 13:01:47 +0500

    making constructors

--- Computer.java
+++ Computer.java
@@ -3,4 +3,17 @@ public class Computer {
     protected int memorySize; //in megabytes
     protected int storageSize; //in megabytes
     protected int speed; //in megahertz
+    public Computer() {
+        wordSize=0;
+        memorySize=0;
+        storageSize=0;
+        speed=0;
+    }
+    public Computer(int wordSize, int memorySize, int storageSize, int speed) {
+        this.wordSize=wordSize;
+        this.memorySize=memorySize;
+        this.storageSize=storageSize;
+        this.speed=speed;
+    }
+    
 }
