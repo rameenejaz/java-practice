e93e37fae9c28a3b3fd58d9d9dac6d96809b3ed8
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-05 12:50:18 +0500

    making a main function

--- Main3.java
+++ Main3.java
@@ -1,6 +1,16 @@
 //files used are: person3, DOB2, Student3 and Address3, Main3
 public class Main3 {
     private Person3 [] person=new Person3[100];
+    public static void displayMenu() {
+        System.out.println("---------- MENU----------");
+        System.out.println("1. Enter the number of people you want to register");
+        System.out.println("2. To input data of the people");
+        System.out.println("3: To edit data of student by entering name");
+        System.out.println("4.To delete a person by entering their name");
+        System.out.println("5: To view list of students month wise"); //ex:display data of all the students when may is entered
+        System.out.println("6: To display list of students city wise."); //ex: display data of all those students in lahore
+        System.out.println("0: To exit");
+    }
     public static void main(String[] args) {
         
     }
