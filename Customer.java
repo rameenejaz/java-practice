2e4114ac6fa5d542c87308fd42e53b7a5d798c6d
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-12 12:06:38 +0500

    making a grocery store functionalities

---
+++ Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+    private int ID;
+    private String name;
+    public Customer() {
+        ID=0;
+        name="";
+    }
+    public Customer(int ID, String name) {
+        this.ID=ID;
+        this.name=name;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID=ID;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name=name;
+    }
+    
+}
---
+++ Date2.java
@@ -0,0 +1,25 @@
+public class Date2 {
+    private int day;
+    private String month;
+    public Date2() {
+        day=0;
+        month="";
+    }
+    public Date2(int day, String month) {
+        this.day=day;
+        this.month=month;
+    }
+    public int getDay() {
+        return day;
+    }
+    public void setDay(int day) {
+        this.day=day;
+    }
+    public String getMonth() {
+        return month;
+    }
+    public void setMonth(String month) {
+        this.month=month;
+    } 
+    
+ }
