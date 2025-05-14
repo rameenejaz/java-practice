221795cd6544da0323b437530727e24f704b5957
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-14 23:54:56 +0500

    overriding the functions

--- VerifiedSimple.java
+++ VerifiedSimple.java
@@ -1,3 +1,38 @@
 public class VerifiedSimple extends Simple{
-    
+    public VerifiedSimple(double number1, double number2) {
+        super(number1, number2);
+    }
+    @Override
+    public void add() {
+        if (number1>0 && number2>0) {
+            super.add();
+        }
+        else {
+            System.out.println("Error: Both numbers must be greater than 0 for addition.");
+        }
+    }
+    @Override
+    public void sub() {
+        if (num1 > 0 && num2 > 0) {
+            super.sub();
+        } else {
+            System.out.println("Error: Both numbers must be greater than 0 for subtraction.");
+        }
+    }
+    public void mul() {
+        if (num1 > 0 && num2 > 0) {
+            super.mul();
+        } else {
+            System.out.println("Error: Both numbers must be greater than 0 for multiplication.");
+        }
+    }
+
+    @Override
+    public void div() {
+        if (num1 > 0 && num2 > 0) {
+            super.div();
+        } else {
+            System.out.println("Error: Both numbers must be greater than 0 for division.");
+        }
+    }
 }
