b54dd48361639f21aac270c79cf9f9ec410efc3c
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-15 01:39:13 +0500

    drawing shapes

--- Circle.java
+++ Circle.java
@@ -1,4 +1,4 @@
-//files used are shape, circle, triangle, and sqaure
+//files used are shape, circle, triangle, and sqaure and main6
 public class Circle extends Shape {
     public Circle(String penColor, String fillColor) {
         // since a circle has zero sides
@@ -7,7 +7,7 @@ public class Circle extends Shape {
     @Override
     public void draw () {
         System.out.println("Drawing a circle");
-        System.out.print("Lines: " +numberOfLines);
+        System.out.println("Lines: " +numberOfLines);
         System.out.println("Pen Color: " + penColor);
         System.out.println("Fill Color: " + fillColor);
         System.out.println("O");
