d6d332aab55e8b933c00659ef9d6da79c720559a
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-15 01:27:17 +0500

    making a few minor changes

--- Square.java
+++ Square.java
@@ -1,3 +1,13 @@
 public class Square extends Shape{
-    
+    public Square(int numberOfLines,String penColor, String fillColor) {
+        super(numberOfLines, penColor, fillColor );
+    }
+    @Override
+    public void draw () {
+        System.out.println("Drawing a circle");
+        System.out.print("Lines: " +numberOfLines);
+        System.out.println("Pen Color: " + penColor);
+        System.out.println("Fill Color: " + fillColor);
+        System.out.println("O");
+    }
 }
