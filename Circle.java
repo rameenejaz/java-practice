45652df1913d8cec9b8f6e3c6851f224ada87fff
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-15 01:25:56 +0500

    adding functionalities in the circle subclass

--- Circle.java
+++ Circle.java
@@ -1,3 +1,15 @@
 public class Circle extends Shape {
-    
+    public Circle(String penColor, String fillColor) {
+        // since a circle has zero sides
+        super(0, penColor, fillColor );
+    }
+    @Override
+    public void draw () {
+        System.out.println("Drawing a circle");
+        System.out.print("Lines: " +numberOfLines);
+        System.out.println("Pen Color: " + penColor);
+        System.out.println("Fill Color: " + fillColor);
+        System.out.println("O");
+    }
+
 }
