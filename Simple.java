b96b198f5ac0f99b5e3bf1357f7ba294d9505613
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-14 23:55:16 +0500

    making some methods

--- Simple.java
+++ Simple.java
@@ -1,15 +1,15 @@
 public class Simple {
-    private double number1;
-    private double number2;
+    protected double number1;
+    protected double number2;
     public Simple() {
-        number1=1l
+        number1=1;
         number2=1;
     }
     public Simple(double number1, double number2) {
         this.number1=number1;
         this.number2=number2;
     }
-    public void add(double number1, double number2) {
+    public void add() {
         double add=number1+number2;
         System.out.println("Addition: " + add);
     }
@@ -21,7 +21,7 @@ public class Simple {
         double multiply=number1*number2;
         System.out.println("Multiplication: " + multiply);
     }
-    public void divide () {
+    public void div () {
         if (number2!=0) {
             double divide=number1/number2;
             System.out.println("Division: " + divide);
