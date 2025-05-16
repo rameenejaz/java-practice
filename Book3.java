0b85ee124c12195f4b92c0757461ebf4635bce55
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-17 00:32:17 +0500

    testing methods in main

--- Book3.java
+++ Book3.java
@@ -13,10 +13,11 @@ public class Book3 {
         this.author=author;
     }
     public void displayBooks() {
-        System.out.println("------------------------");
+        System.out.println("---------------------------");
         System.out.println("Name: " + name);
         System.out.println("Publisher:" + publisher);
         System.out.println("Author:" + author);
-        System.out.println("------------------------");
+        System.out.println("---------------------------");
+        System.out.println();
     }
 }
--- BookStore.java
+++ BookStore.java
@@ -1,10 +1,10 @@
 //using files book3 and bookStore
 public class BookStore {
-    Book3 book1=new Book3();
-    Book3 book2=new Book3();
-    Book3 book3=new Book3();
-    Book3 book4=new Book3();
-    Book3 book5=new Book3();
+    Book3 book1=new Book3("Java how to code", "Pearson", "John");
+    Book3 book2=new Book3("Harry Potter", "UK Print Press", "JK Rowling");
+    Book3 book3=new Book3("Die for me", "China Town", "Rameen");
+    Book3 book4=new Book3("History of Pakistan", "Pakistan Gov", "Army");
+    Book3 book5=new Book3("Geography", "ABC school", "Mr dani");
 
     public void displayAll() {
         book1.displayBooks();
@@ -12,5 +12,9 @@ public class BookStore {
         book3.displayBooks();
         book4.displayBooks();
         book5.displayBooks();
+    }
+    public static void main(String[] args) {
+        BookStore books=new BookStore();
+        books.displayAll();
     }
 }
