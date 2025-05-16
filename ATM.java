08f16f360a5453a0308c51bd78f2bea66c20ad71
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-17 01:17:44 +0500

    making methods

--- ATM.java
+++ ATM.java
@@ -1,5 +1,6 @@
 public class ATM {
     public static void main(String[] args) {
+        Account2 account=new Account2();
         
     }
 }
--- Account2.java
+++ Account2.java
@@ -12,34 +12,47 @@ public class Account2 {
     }
     public void withdraw() {
         Scanner input=new Scanner(System.in);
-        System.out.print("Enter the amount you want to withdraw:");
-        int amount=input.nextInt();
-        input.nextLine();
-        if (amount<0) {
-            System.err.println("Invalid amount!");
-            return;
-        }
-        if (amount>=0 && amount<balance) {
-            balance-=amount;
-            System.out.println(amount +" withdrawn, your new balance is: ", balance);
+        System.out.print("Enter the account number you want to perform operation on:");
+        String inputAccountNum=input.nextLine();
+        if (inputAccountNum.equals(inputAccountNum)) {
+            System.out.print("Enter the amount you want to withdraw:");
+            int amount=input.nextInt();
+            input.nextLine();
+            if (amount<0) {
+                System.err.println("Invalid amount!");
+                return;
+            }
+            if (amount>=0 && amount<balance) {
+                balance-=amount;
+                System.out.println(amount +" withdrawn, your new balance is: ", balance);
+            }
         }
     }
     public void deposit() {
         Scanner input=new Scanner(System.in);
+        System.out.print("Enter the account number you want to perform operation on:");
+        String inputAccountNum=input.nextLine();
+        if (inputAccountNum.equals(inputAccountNum)) {
         System.out.print("Enter the amount you want to deposit:");
         int amount=input.nextInt();
         input.nextLine();
-        if (amount<0) {
+            if (amount<0) {
             System.err.println("Invalid amount!");
             return;
-        }
-        if (amount>=0 && amount<balance) {
+            }
+            if (amount>=0 && amount<balance) {
             balance+=amount;
             System.out.println(amount +" deposited, your new balance is: ", balance);
+            } 
         }
     }
     public double balanceInquiry() {
-        return balance;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the account number you want to perform operation on:");
+        String inputAccountNum=input.nextLine();
+        if (inputAccountNum.equals(inputAccountNum)) {
+            return balance;
+        }
     }
 
 
