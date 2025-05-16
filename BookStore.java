1bc6141e58e43df9063f38508859cadd85f9b929
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-16 23:55:42 +0500

    making five book3 objects

---
+++ BookStore.java
@@ -0,0 +1,8 @@
+//using files book3 and bookStore
+public class BookStore {
+    Book3 book1=new Book3();
+    Book3 book2=new Book3();
+    Book3 book3=new Book3();
+    Book3 book4=new Book3();
+    Book3 book5=new Book3();
+}
