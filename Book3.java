9ae5a836f7a97d552047b7d1cbd42927cf72867e
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-16 23:54:56 +0500

    making constructors

--- Book3.java
+++ Book3.java
@@ -2,5 +2,14 @@ public class Book3 {
     private String name;
     private String publisher;
     private String person;
-    
+    public Book3() {
+        name="";
+        publisher="";
+        person="";
+    }
+    public Book3(String name, String publisher, String person) {
+        this.name=name;
+        this.publisher=publisher;
+        this.person=person;
+    }
 }
