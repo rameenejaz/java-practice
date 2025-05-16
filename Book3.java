54f51d05444a1a18da584c2f5573c37fa5c5c51f
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-17 00:23:19 +0500

    making a display function

--- Book3.java
+++ Book3.java
@@ -1,15 +1,22 @@
 public class Book3 {
     private String name;
     private String publisher;
-    private String person;
+    private String author;
     public Book3() {
         name="";
         publisher="";
-        person="";
+        author="";
     }
-    public Book3(String name, String publisher, String person) {
+    public Book3(String name, String publisher, String author) {
         this.name=name;
         this.publisher=publisher;
-        this.person=person;
+        this.author=author;
+    }
+    public void displayBooks() {
+        System.out.println("------------------------");
+        System.out.println("Name: " + name);
+        System.out.println("Publisher:" + publisher);
+        System.out.println("Author:" + author);
+        System.out.println("------------------------");
     }
 }
