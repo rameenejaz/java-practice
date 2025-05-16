c59de9ab7f0960323641d4c9abd70023db034ff3
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-17 00:24:41 +0500

    making a display function

--- BookStore.java
+++ BookStore.java
@@ -5,4 +5,12 @@ public class BookStore {
     Book3 book3=new Book3();
     Book3 book4=new Book3();
     Book3 book5=new Book3();
+
+    public void displayAll() {
+        book1.displayBooks();
+        book2.displayBooks();
+        book3.displayBooks();
+        book4.displayBooks();
+        book5.displayBooks();
+    }
 }
