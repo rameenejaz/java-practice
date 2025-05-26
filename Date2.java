eaec9da4a648ba191f5726d0094c61db1c2d9b04
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-26 21:12:16 +0500

    making some changes

--- Date2.java
+++ Date2.java
@@ -2,13 +2,16 @@
 public class Date2 {
     private int day;
     private String month;
+    private int year;
     public Date2() {
         day=0;
         month="";
+        year=0;
     }
-    public Date2(int day, String month) { //parameterized constructor
+    public Date2(int day, String month, int year) { //parameterized constructor
         this.day=day;
         this.month=month;
+        this.year=year;
     }
     private boolean isValidMonth(String month) {
         String [] months={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
@@ -31,9 +34,14 @@ public class Date2 {
     public void setMonth(String month) {
         this.month=month;
     }
+    public void setYear(int year) {
+        this.year=year;
+    }
+    public int getYear() {
+        return year;
+    }
     public String toString() {
         return day +"/" + month + "/" + year;
     }
-    
 
  }
