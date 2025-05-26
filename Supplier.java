285781005d44ec31db0bb73677e2c22307dbe6a9
Rameen Ejaz <rameenejaz4@gmail.com>
2025-05-26 12:12:58 +0500

    making a supplier class and keeping track of orders

---
+++ Supplier.java
@@ -0,0 +1,15 @@
+public class Supplier {
+    private int ID;
+    private String name;
+    private String city;
+    public Supplier() {
+        ID=0;
+        name="";
+        city="";
+    }
+    public Supplier(int ID, String name, String city) {
+        this.ID=ID;
+        this.name=name;
+        this.city=city;
+    }
+}
