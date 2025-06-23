97cdd2ad35dd0fec4e589ffc3453e7747b8eceab
Rameen Ejaz <rameenejaz4@gmail.com>
2025-06-23 23:45:51 +0500

    adding a few new classes

--- Book4.java
+++ Book4.java
@@ -1,3 +1,32 @@
 public class Book4 {
-    
-}
+    private String title;
+    private String author;
+    private int ID;
+    private boolean issued;
+    public int getID() {
+        return ID;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String author () {
+        return author;
+    }
+    public Book4() {
+        title="";
+        author="";
+        ID=0;
+    }
+    public Book4(String title, String author, int ID) {
+        this.title=title;
+        this.author=author;
+        this.ID=ID;
+    }
+    public boolean isIssued() {
+        return issued;
+    }
+    public void isIssued(boolean issused) {
+        this.issued=issused;
+    }
+
+ }
