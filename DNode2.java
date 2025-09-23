6f2995c5f67935096be5a93aa31326621739d32e
Rameen Ejaz <rameenejaz4@gmail.com>
2025-09-23 16:10:02 +0500

    making a doubly linked list class

---
+++ DNode2.java
@@ -0,0 +1,11 @@
+public class DNode2 {
+    int data;
+    DNode2 next;
+    DNode2 previous;
+    public DNode2(int data) {
+        this.data=data;
+        this.previous=null;
+        this.next=null;
+    }
+    
+}
