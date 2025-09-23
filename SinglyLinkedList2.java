cbd7af689e581350c165678c8eb777a9bf6c6edc
Rameen Ejaz <rameenejaz4@gmail.com>
2025-09-23 14:33:51 +0500

    insertion at start

---
+++ SinglyLinkedList2.java
@@ -0,0 +1,9 @@
+public class SinglyLinkedList2 {
+    Node head;
+    public void insertAtStart(int value) {
+        Node newNode=new Node(value);
+        newNode.next=head;
+        head=newNode;
+    }
+    
+}
