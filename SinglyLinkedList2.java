c4448de90c14ee9a4e3c54a8efddc473fde049f5
Rameen Ejaz <rameenejaz4@gmail.com>
2025-09-23 14:40:28 +0500

    insertion at end of the linked list

--- SinglyLinkedList2.java
+++ SinglyLinkedList2.java
@@ -1,11 +1,20 @@
 public class SinglyLinkedList2 {
     Node head;
+    Node tail;
     public void insertAtStart(int value) {
         Node newNode=new Node(value);
         newNode.next=head;
         head=newNode;
     }
     public void insertAtEnd(int value) {
-       
+        Node newNode=new Node(value);
+            if (head==null) {
+                head=newNode;
+                return;
+            }
+          newNode.data=value;
+          newNode.next=null;
+          tail=newNode;
+           
     }
 }
