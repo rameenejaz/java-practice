3df51c80a8857fc3f1203c90870478de07b3551b
Rameen Ejaz <rameenejaz4@gmail.com>
2025-09-23 14:44:48 +0500

    insertion at specific position of singly linked list

--- SinglyLinkedList2.java
+++ SinglyLinkedList2.java
@@ -1,20 +1,36 @@
 public class SinglyLinkedList2 {
-    Node head;
-    Node tail;
+    Node2 head;
+    Node2 tail;
     public void insertAtStart(int value) {
-        Node newNode=new Node(value);
+        Node2 newNode=new Node2(value);
         newNode.next=head;
         head=newNode;
     }
     public void insertAtEnd(int value) {
-        Node newNode=new Node(value);
+        Node2 newNode=new Node2(value);
             if (head==null) {
                 head=newNode;
                 return;
             }
           newNode.data=value;
           newNode.next=null;
-          tail=newNode;
-           
+          tail=newNode;  
+    }
+    public void insertAtSpecific(int pos, int value) {
+        Node2 temp=new Node2(value);
+        Node2 previous=new Node2(value);
+        Node2 current=new Node2(value);
+        if (pos==1) {
+            insertAtStart(value);
+        }
+        current=head;
+        for (int i=1; i<pos; i++) {
+            previous=current;
+            current=current.next;
+        }
+        temp.data=value;
+        previous.next=temp;
+        temp.next=current;
+
     }
 }
