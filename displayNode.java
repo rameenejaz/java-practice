706171fe8d60cfa011d61b80ddd7b58304eb1de4
Rameen Ejaz <rameenejaz4@gmail.com>
2025-09-28 19:33:25 +0500

    insert at specific position

--- displayNode.java
+++ displayNode.java
@@ -24,6 +24,24 @@ public class displayNode {
         newNode.next=head;
         head=newNode;
     }
+    public void insertAtSpecific(int position, int value) {
+        Node3 newNode=new Node3(value);
+        if (position==1) {
+            newNode.next=head;
+            head=newNode;
+            return;
+        }
+        Node3 temp=head;
+        for (int i=0; i<position-1 && temp!=null; i++) {
+            temp=temp.next;
+        }
+        if (temp==null) {
+            System.out.println("Out of range");
+            return;
+        }
+        newNode.next=head.next;
+        temp.next=newNode;
+    }
     public void display() {
         Node3 temp=head;
         while (temp!=null) {
@@ -42,6 +60,9 @@ public class displayNode {
 
        list.insertAtStart(5);
        System.out.print("New Linked List:");
+        list.display();
+
+        list.insertAtSpecific(2, 65);
         list.display();
     }
     
